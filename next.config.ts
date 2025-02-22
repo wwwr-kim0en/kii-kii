b8d53025eedf552f66f0486d05e30b76b1291a96
김영은 <jessicak0828@gmail.com>
2025-02-22 12:28:15 +0900

    config: 개발 환경 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	reactStrictMode: false,
+	// 연결되어 있는 도메인이 확정되지 않은 관계로 모든 hostName에 대한 접근을 임시로 허용함
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**',
+			},
+		],
+	},
+	// 실 데이터가 들어간 이후 설정 필요
+	// images: {
+	//   domains: ["localhost", "*", "storage.googleapis.com", "picsum.photos"],
+	// },
 };
 
 export default nextConfig;
