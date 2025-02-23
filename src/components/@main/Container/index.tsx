95d8836dac0474678a4f4fc35611e346838ff9ca
김영은 <jessicak0828@gmail.com>
2025-02-23 12:37:16 +0900

    delete: query 관련 커스텀 훅 삭제
    api 요청에 대한 로직들을 tanstack query로 분리하려고 했으나 일단 보류

--- src/components/@main/Container/index.tsx
+++ src/components/@main/Container/index.tsx
@@ -1,9 +1,42 @@
 'use client';
 
-import useGetListItems from '@/hooks/api/useGetListItems';
+import { Button } from '@/components/ui/button';
 
-export default function Container({ user }) {
-	if (!user) return;
-	const res = useGetListItems(user);
-	return <div></div>;
+import supabase from '@/lib/supabase/client';
+import { User } from '@supabase/supabase-js';
+import { useEffect, useState } from 'react';
+
+export default function Container({ user }: { user: User | null }) {
+	const [folderLists, setFolderLists] = useState<any[]>([]);
+	function sortFoldersByLatest() {
+		if (!folderLists.length) return;
+		const sorted = folderLists.sort((a, b) => a.created_at - b.created_at);
+		setFolderLists(sorted);
+	}
+	useEffect(() => {
+		supabase
+			.from('folder_d0')
+			.select('*')
+			.eq('author_id', user?.id)
+			.then((res) => {
+				const { data, count, error, status, statusText } = res;
+				console.log('list', data);
+				if (!data) return;
+				const foldersSortedByIndex = data.sort((a, b) => a.index - b.index);
+				setFolderLists(foldersSortedByIndex);
+			});
+	}, []);
+	return (
+		<div>
+			<div>
+				<Button onClick={sortFoldersByLatest}>최신순 정렬</Button>
+			</div>
+			<p>count: {folderLists.length}개</p>
+			<ul>
+				{folderLists.map((folder, index) => (
+					<li key={folder.id}>{folder.name}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
