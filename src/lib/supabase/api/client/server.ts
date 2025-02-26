d79647ec0d0fd9aebcd0630408b673e28d6a3a26
김영은 <jessicak0828@gmail.com>
2025-02-26 09:47:21 +0900

    feat & fix: separate Server & Client-side when using useClient(useServerClient)

---
+++ src/lib/supabase/api/client/server.ts
@@ -0,0 +1,33 @@
+'use server';
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
+if (!supabaseUrl || !supabaseKey) {
+	throw new Error('env missing : Supabase URL,Supabase Anon Key 없습니다.');
+}
+
+export default async function getServerClient() {
+	const cookieStore = await cookies();
+	return createServerClient(supabaseUrl!, supabaseKey!, {
+		auth: {
+			flowType: 'pkce',
+		},
+		cookies: {
+			getAll() {
+				return cookieStore.getAll();
+			},
+			setAll(cookiesToSet) {
+				try {
+					cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
+				} catch {
+					// The `setAll` method was called from a Server Component.
+					// This can be ignored if you have middleware refreshing
+					// user sessions.
+				}
+			},
+		},
+	});
+}
