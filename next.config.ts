8c7f2353ec5d414938604e18bd241755e30932eb
김영은 <jessicak0828@gmail.com>
2025-02-26 09:50:43 +0900

    fix: fix error - no existing atrribute

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	reactStrictMode: false,
+
 	// 연결되어 있는 도메인이 확정되지 않은 관계로 모든 hostName에 대한 접근을 임시로 허용함
 	images: {
 		remotePatterns: [
@@ -15,6 +16,9 @@ const nextConfig: NextConfig = {
 	// images: {
 	//   domains: ["localhost", "*", "storage.googleapis.com", "picsum.photos"],
 	// },
+	experimental: {
+		// nodeMiddleware: true,
+	},
 };
 
 export default nextConfig;
